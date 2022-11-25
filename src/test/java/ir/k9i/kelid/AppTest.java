92d6ba4ce6fc047a85836f0061c78211eb87df76
Hamidreza Khosroabadi <hamidrezakhosroabadi@outlook.com>
2022-11-25 16:40:03 +0330

    - Test template was added!

---
+++ src/test/java/ir/k9i/kelid/AppTest.java
@@ -0,0 +1,20 @@
+package ir.k9i.kelid;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
