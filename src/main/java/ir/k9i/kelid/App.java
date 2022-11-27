b4800f8037f3505f8f63b15a7d01d38ca971779d
Hamidreza Khosroabadi <hamidrezakhosroabadi@outlook.com>
2022-11-27 23:10:14 +0330

    - Get spots route was implemented!

--- src/main/java/ir/k9i/kelid/App.java
+++ src/main/java/ir/k9i/kelid/App.java
@@ -1,6 +1,16 @@
 package ir.k9i.kelid;
 
+import static spark.Spark.*;
+import ir.k9i.kelid.Node.Node;
+import ir.k9i.kelid.Ring.Ring;
+
 public class App {
     public static void main(String[] args) {
+        Ring.push(new Node((int) Math.random() + 1, "main", "http://localhost:4567/"));
+
+        get("/keys/:key/spots", (request, response) -> {
+            return Ring.getSpots(request.params(":key").hashCode(), Integer.parseInt(request.queryParams("count")));
+        });
+
     }
 }
